a4b9ffbb1243e318251d83412d3ce63919f54c04
Alerodrigues11 <alexandre.rodriguesmaiafilho@gmail.com>
2022-12-29 23:28:40 -0300

    Coding the edit page

--- editar-produtos.php
+++ editar-produtos.php
@@ -1,4 +1,5 @@
 <?php
+    include_once("helpers/url.php");
     include_once("templates/header.php");
     if(empty($_SESSION["Status"]) || $_SESSION["Status"] != 1 ) {
         header("Location:index.php");
@@ -11,25 +12,26 @@
             <input type="hidden" name="id" value="<?=$produto["id"]?>">
             <div class="form-group">
                 <label for="nomeproduto">Nome do Produto:</label>
-                <input type="text" class="form-control" id="nomeproduto" name="nomeproduto" value="<?= $produto["nome"] ?>" required>
+                <input type="text" class="form-control" id="nomeproduto" name="nomeproduto" value="<?= $produto["nome"] ?>">
             </div>
             <div class="form-group">
                 <label for="precoproduto">Preço:</label>
-                <input type="text" class="form-control" id="precoproduto" name="precoproduto" required>
+                <input type="text" class="form-control" id="precoproduto" name="precoproduto" value="<?= $produto["preco"] ?>">
             </div>
             <div class="form-group">
                 <label for="estoque">Quantidade Disponível:</label>
-                <input type="text" class="form-control" id="estoque" name="estoque" required>
+                <input type="text" class="form-control" id="estoque" name="estoque" value="<?= $produto["estoque"] ?>">
             </div>
             <div class="form-group">
                 <label for="descricaoproduto">Descrição do Produto:</label>
-                <input type="text" class="form-control" id="descricaoproduto" name="descricaoproduto" required>
+                <input type="text" class="form-control" id="descricaoproduto" name="descricaoproduto" value="<?= $produto["descricao"] ?>">
             </div>
             <div class="form-group">
                 <label for="imagemproduto">Arquivo de imagem:</label>
-                <input type="file" class="form-control" id="imagemproduto" name="imagemproduto" required>
+                <input type="file" class="form-control" id="imagemproduto" name="imagemproduto">
+                <img class="img-edit-products" src="<?= $BASE_URL?>/upload/<?= $produto["imagem"]?>" alt="Imagem do produto">
             </div>
-                <button type="submit" class="w-100 btn btn-primary">Cadastrar</button>
+                <button type="submit" class="w-100 btn btn-primary">Editar</button>
         </form>
     </div>
 <?php
--- helpers/process_db_cadastro_produtos.php
+++ helpers/process_db_cadastro_produtos.php
@@ -54,14 +54,68 @@
             $error = $e->getMessage();
             echo "Erro: $error";
         }
-
-        header("Location: ". $BASE_URL . "../index.php");
+        } else if($data["type"] === "edit")
+        {
+            $nomeProduto = $data["nomeproduto"];
+            $precoProduto = $data["precoproduto"];
+            $estoque = $data["estoque"];
+            $descricaoProduto = $data["descricaoproduto"];
+            $id = $data["id"];
+            $img = $_FILES["imagemproduto"];
+            
+            try {
+                $procura = "SELECT imagem FROM produtos WHERE id = :id";
+                $seleciona = $conn_clients->prepare($procura);
+                $seleciona->bindParam(":id", $id);
+                $seleciona->execute();
+    
+                $exibe = $seleciona->fetch();
+            } catch (PDOException $e) {
+                $error = $e->getMessage();
+                echo "Erro: $error";
+            }
+    
+            if(!empty($img["name"])) 
+            {
+                $extensao = pathinfo($img["name"], PATHINFO_EXTENSION);
+                $imagemNome = md5(uniqid($img["name"])).".".$extensao;
+                $uploadFoto=1;
+            } else {
+                $imagemNome = $exibe["imagem"];
+                $uploadFoto=0;
+            }
+    
+            if ($uploadFoto == 1) {
+                $diretorio = "../upload/";
+                move_uploaded_file($img["tmp_name"], $diretorio.$imagemNome);
+            }
+    
+            $query = "UPDATE produtos SET nome = :nomeProduto, preco = :precoProduto, estoque = :estoque, descricao = :descricaoProduto, imagem = :imagemNome WHERE id = :id";
+    
+            $stmt = $conn_clients->prepare($query);
+            
+            $stmt->bindParam(":nomeProduto", $nomeProduto);
+            $stmt->bindParam(":precoProduto", $precoProduto);
+            $stmt->bindParam(":estoque", $estoque);
+            $stmt->bindParam(":descricaoProduto", $descricaoProduto);
+            $stmt->bindParam(":imagemNome", $imagemNome);
+            $stmt->bindParam(":id", $id);
+    
+            try {
+                $stmt->execute();
+                $_SESSION["msg"] = "Produto atualizado com sucesso!";
+    
+                
+            } catch (PDOException $e) {
+                $error = $e->getMessage();
+                echo "Erro: $error";
+            }
         }
 
-        
-    } else 
+        header("Location: ". $BASE_URL . "../editar-lista-produtos.php");
+
+    } else {
     
-    {
         $id;
     
         if(!empty($_GET)) 
@@ -94,5 +148,3 @@
     }
 
 }
-
-$conn = null;
